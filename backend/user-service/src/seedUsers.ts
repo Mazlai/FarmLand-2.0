b8ec31330b18a18317eef19c0e61d963f2bcbc75
Mickael Fernandez <57178136+Mazlai@users.noreply.github.com>
2026-03-20 14:26:23 +0100

    fix: usage of ESLint in each microservice

--- backend/user-service/src/seedUsers.ts
+++ backend/user-service/src/seedUsers.ts
@@ -3,32 +3,35 @@ import sequelize from "./config/database";
 import User from "./models/user.models";
 
 async function seed() {
-    try {
-        await sequelize.sync();
+  try {
+    await sequelize.sync();
 
-        const availableGenders = ['male', 'female'];
+    const availableGenders = ["male", "female"];
 
-        const userData = [];
-        for (let i = 0; i < 10; i++) {
-            userData.push({
-                firstName: faker.person.firstName(),
-                lastName: faker.person.lastName(),
-                birthDate: faker.date.anytime(),
-                email: faker.internet.email(),
-                phone: faker.phone.number(),
-                gender: availableGenders[Math.floor(Math.random() * availableGenders.length)] ?? '',
-                passwordHash: faker.internet.password()
-            });
-        }
-
-        await User.bulkCreate(userData);
-
-        console.log("Seed done: 10 users created");
-        process.exit(0);
-    } catch (err) {
-        console.error("Seed error:", err);
-        process.exit(1);
+    const userData = [];
+    for (let i = 0; i < 10; i++) {
+      userData.push({
+        firstName: faker.person.firstName(),
+        lastName: faker.person.lastName(),
+        birthDate: faker.date.anytime(),
+        email: faker.internet.email(),
+        phone: faker.phone.number(),
+        gender:
+          availableGenders[
+            Math.floor(Math.random() * availableGenders.length)
+          ] ?? "",
+        passwordHash: faker.internet.password(),
+      });
     }
+
+    await User.bulkCreate(userData);
+
+    console.log("Seed done: 10 users created");
+    process.exit(0);
+  } catch (err) {
+    console.error("Seed error:", err);
+    process.exit(1);
+  }
 }
 
 seed();

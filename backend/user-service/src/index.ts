1f32dee0a183a4b7fe7bb0cc16f3a02ec9087af6
Mickael Fernandez <57178136+Mazlai@users.noreply.github.com>
2026-04-07 09:50:04 +0200

    docs: update swagger title on develop

--- backend/user-service/src/index.ts
+++ backend/user-service/src/index.ts
@@ -6,7 +6,7 @@ import logger from "./utils/logger";
 const swaggerOptions = {
   swaggerDefinition: {
     info: {
-      title: "FarmLand user API doc.",
+      title: "FarmLand User API - V2",
       version: "1.0.0",
       description: "FarmLand user API documentation.",
     },
